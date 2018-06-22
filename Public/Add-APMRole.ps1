2c6f6bb322d85c7c5c9c96e78292359b2f999cb6
Klein <551479@bah.com>
2018-06-22 11:18:48 -0400

    Added documentation to Add-APMRole

--- Public/Add-APMRole.ps1
+++ Public/Add-APMRole.ps1
@@ -1,97 +1,37 @@
 ﻿Function Add-APMRole {
 <#
 .SYNOPSIS
-    Adds a single ACL entry to existing ACL Role Object
+    Adds a single ACL entry to existing ACL Role Object.  This function is hard coded for LDAP lookups. Change as needed.
 
 .DESCRIPTION
-    A detailed description of the function or script.
+F5 stores VPN user ACL to LDAP role mappings in what they call an aggreagte reasource assign group.
+
+In our shop we link ACL permissions to LDAP user groups.  This will append 
+'expression' = "expr { [mcget {session.ldap.last.attr.memberOf}] contains "CN=ldapgroupname," }
+to the array of mappings tied to each specific ACL.
 
 .PARAMETER name
 
+The name of the aggregate reasrouce group assigned to the VPN access profile.
+These can be found at the REST endpoint /apm/policy/agent/resource-assign/
+
+As of 6/18 dev was using "aggregate_acl_act_full_resource_assign_ag" and prod was using acl_1_act_full_resource_assign_ag
+
 .PARAMETER acl
+
+The existing ACL we want to map and LDAP group to.
 
 .PARAMETER group
 
+The existing LDAP group we want to map to and ACL
 
 .EXAMPLE
 
-.EXAMPLE
+Add mapping for ACL myACL on the prod F5 to my_LDAPgroup
 
-.EXAMPLE
+Add-APMRole -name "acl_1_act_full_resource_assign_ag" -acl "myACL" -group "my_LDAPgroup"
 
-.INPUTS
-    The Microsoft .NET Framework types of objects that can be piped to the
-    function or script. You can also include a description of the input
-    objects.
 
-.OUTPUTS
-    The .NET Framework type of the objects that the cmdlet returns. You can
-    also include a description of the returned objects.
-
-.NOTES
-    Additional information about the function or script.
-
-.LINK
-
-.LINK
-
-.COMPONENT
-    The technology or feature that the function or script uses, or to which
-    it is related.
-
-.ROLE
-    The user role for the help topic. This content appears when the Get-Help
-    command includes the Role parameter of Get-Help.
-
-.FUNCTIONALITY
-    The intended use of the function. This content appears when the Get-Help
-    command includes the Functionality parameter of Get-Help.
-
-.FORWARDHELPTARGETNAME <Command-Name>
-    Redirects to the help topic for the specified command. You can redirect
-    users to any help topic, including help topics for a function, script,
-    cmdlet, or provider.
-
-.FORWARDHELPCATEGORY  <Category>
-    Specifies the help category of the item in ForwardHelpTargetName.
-    Valid values are Alias, Cmdlet, HelpFile, Function, Provider, General,
-    FAQ, Glossary, ScriptCommand, ExternalScript, Filter, or All. Use this
-    keyword to avoid conflicts when there are commands with the same name.
-
-.REMOTEHELPRUNSPACE <PSSession-variable>
-    Specifies a session that contains the help topic. Enter a variable that
-    contains a PSSession. This keyword is used by the Export-PSSession
-    cmdlet to find the help topics for the exported commands.
-
-.EXTERNALHELP  <XML Help File>
-    Specifies an XML-based help file for the script or function.
-
-    The ExternalHelp keyword is required when a function or script
-    is documented in XML files. Without this keyword, Get-Help cannot
-    find the XML-based help file for the function or script.
-
-    The ExternalHelp keyword takes precedence over other comment-based
-    help keywords. If ExternalHelp is present, Get-Help does not display
-    comment-based help, even if it cannot find a help topic that matches
-    the value of the ExternalHelp keyword.
-
-    If the function is exported by a module, set the value of the
-    ExternalHelp keyword to a file name without a path. Get-Help looks for
-    the specified file name in a language-specific subdirectory of the module
-    directory. There are no requirements for the name of the XML-based help
-    file for a function, but a best practice is to use the following format:
-    <ScriptModule.psm1>-help.xml
-
-    If the function is not included in a module, include a path to the
-    XML-based help file. If the value includes a path and the path contains
-    UI-culture-specific subdirectories, Get-Help searches the subdirectories
-    recursively for an XML file with the name of the script or function in
-    accordance with the language fallback standards established for Windows,
-    just as it does in a module directory.
-
-    For more information about the cmdlet help XML-based help file format,
-    see "How to Create Cmdlet Help" in the MSDN (Microsoft Developer Network)
-    library at http://go.microsoft.com/fwlink/?LinkID=123415.
 #>
 
     [cmdletBinding()]
