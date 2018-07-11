e5e5653a3187117060efee835994e0ff3604d309
Klein <551479@bah.com>
2018-07-11 16:05:04 -0400

    New Function to return next free Acl order number

---
+++ Public/Get-NexAclOrder.ps1
@@ -0,0 +1,25 @@
+﻿Function Get-NextAclOrder {
+<#
+.SYNOPSIS
+    Returns the acl order number one less than 10,000
+.NOTES
+   
+    Requires F5-LTM modules from github
+#>
+
+
+    begin {
+        #Test that the F5 session is in a valid format
+        Test-F5Session($F5Session)       
+    }
+    process {
+       
+            $uri = $F5Session.BaseURL.Replace('/ltm/','/apm/acl/') 
+            $acl = Invoke-RestMethodOverride -Method Get -Uri $URI -WebSession $F5Session.WebSession
+            $sorted = $acl.items.aclorder | Sort-Object
+            #return 2nd to last item as we have a deny at acl order 10000
+            $sorted[-2]
+        }
+        
+}
+
