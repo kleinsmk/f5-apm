edd3792239231ef2bf6fd699cae2a7ced009d7da
Klein <551479@bah.com>
2018-07-30 15:17:35 -0400

    including the build file

---
+++ Build-f5-apm.ps1
@@ -0,0 +1,5 @@
+﻿
+Invoke-psake .\psakefile.ps1 -Verbose
+Step-ModuleVersion -Path .\F5-apm.psd1
+Invoke-PSDeploy .\f5-apm.PSDeploy.ps1 -Verbose
+#Publish-Module -Name f5-apm -NuGetApiKey 8b82dc58-4b19-4e17-a3a9-cd9081557914
