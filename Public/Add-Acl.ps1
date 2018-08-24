4eef4e25b55cf051a9afb2cfea96c02c2207ca33
Klein <551479@bah.com>
2018-08-24 09:37:44 -0400

    Changing fucntion to allow for multiple input parameters

--- Public/Add-Acl.ps1
+++ Public/Add-Acl.ps1
@@ -14,15 +14,15 @@
 
 .PARAMETER dstStartPort
 
-    Destination start port
+    Destination start port.  Can take multiples values in csv list format. Ex: 80,8080,443,8443
 
 .PARAMETER dstEndPort
 
-    Desintation end port
+    Desintation end port.  Only mandatory for adding in port ranges.
 
 .PARAMETER dstSubnet
 
-    Destination subnet in format 192.168.1.1/32
+    Destination subnet in format 192.168.1.1/32. Can take multiples values in csv list format. Ex: 10.22.33.200/24,10.224.30.2/28
     Single ip ACL changes are represented by /32 
     Larger network ranges can be used by passing the correct CIDR notation.
 
@@ -40,6 +40,10 @@
     Add-Acl -name Existing_ACL_Name -action allow -dstStartPort 80 -dstEndPort 80 -dstSubnet 192.168.1.1/32
 
     Adds a New ACL for port 80 to a SINGLE IP
+.Example
+    Add-Acl -name Existing_ACL_Name -action allow -dstStartPort 80,8080,443 -dstSubnet 192.168.1.1/32,192.168.1.40/24
+
+    Adds a New ACL for ports 80,8080,443 on Subnets 192.168.1.1/32 and 192.168.1.40/24
 
 #>
     [cmdletBinding()]
@@ -47,89 +51,98 @@
         
         [Alias("Existing acl Name")]
         [Parameter(Mandatory=$true)]
-        [string[]]$name='',
+        [string]$name='',
 
         [Alias("Allow or Deny")]
         [Parameter(Mandatory=$true)]
-        [string[]]$action='',
+        [string]$action='',
 
         [Alias("DestinationStart")]
         [Parameter(Mandatory=$true)]
-        [string[]]$dstStartPort='',
+        [string []]$dstStartPort='',
 
         [Alias("DestinationEnd")]
-        [Parameter(Mandatory=$true)]
-        [string[]]$dstEndPort='',
+        [Parameter(Mandatory=$false)]
+        [string []]$dstEndPort= $dstStartPort,
 
         [Alias("Subnet")]
         [Parameter(Mandatory=$true)]
-        [string[]]$dstSubnet=''
+        [string []]$dstSubnet=''
 
 
     )
     begin {
+        if( $F5Session.WebSession.Headers.'Token-Expiration' -lt (date) ){
+            Write-Warning "F5 Session Token is Expired.  Please re-connect to the F5 device."
+            break
+
+        }
         #Test that the F5 session is in a valid format
         Test-F5Session($F5Session)
         $acl = Get-SingleAcl -name $name
-
-
-
-
     }
+
     process {
-        foreach ($itemname in $Name) {
-            #build
 
-        $baseAclEntry =  [PSCustomObject]@{
-	        'action' = "$action"
-	        'dstEndPort' = "$dstEndPort"
-	        'dstStartPort' = "$dstStartPort"
-	        'dstSubnet' = "$dstSubnet"
-	        'log' = 'packet'
-	        'protocol' = 6
-	        'scheme' = 'any'
-	        'srcEndPort' = 0
-	        'srcStartPort' = 0
-	        'srcSubnet' = '0.0.0.0/0'
-        }
+        foreach ($sub in $dstSubnet) {
 
-            # append the entires if acls already exist
-            if ( -not $acl.entries ) { 
+            foreach ($port in $dstStartPort) {
+                
+                $index = $dstStartPort.IndexOf($port)
+
+                $baseAclEntry =  [PSCustomObject]@{
+	                'action' = "$action"
+	                'dstEndPort' = $dstEndPort[$index]
+	                'dstStartPort' = "$port"
+	                'dstSubnet' = "$sub"
+	                'log' = 'packet'
+	                'protocol' = 6
+	                'scheme' = 'any'
+	                'srcEndPort' = 0
+	                'srcStartPort' = 0
+	                'srcSubnet' = '0.0.0.0/0'
+                }
+
+                # add first acl
+                if ( -not $acl.entries ) { 
             
 
-                $baseAclEntry = [PSCustomObject]@{'entries' = @(
-		                [PSCustomObject]@{
-			            'action' = "$action"
-			            'dstEndPort' = "$dstEndPort"
-			            'dstStartPort' = "$dstStartPort"
-			            'dstSubnet' = "$dstSubnet"
-			            'log' = 'packet'
-			            'protocol' = 6
-			            'scheme' = 'any'
-			            'srcEndPort' = 0
-			            'srcStartPort' = 0
-			            'srcSubnet' = '0.0.0.0/0'}
-	                )}
+                    $baseAclEntry = [PSCustomObject]@{'entries' = @(
+		                    [PSCustomObject]@{
+			                'action' = "$action"
+			                'dstEndPort' = $dstEndPort[$index]
+			                'dstStartPort' = "$port"
+			                'dstSubnet' = $sub
+			                'log' = 'packet'
+			                'protocol' = 6
+			                'scheme' = 'any'
+			                'srcEndPort' = 0
+			                'srcStartPort' = 0
+			                'srcSubnet' = '0.0.0.0/0'}
+	                    )}
 
-                    $JSONBody = $baseAclEntry | ConvertTo-Json -Depth 10
+                        Add-Member -InputObject $acl -NotePropertyName entries -NotePropertyValue (New-Object System.Collections.ArrayList)  #add first acl into object so we don't hit this block again
+                        $acl.entries += $baseAclEntry.entries 
+                        $JSONBody = $baseAclEntry | ConvertTo-Json -Depth 10
             
-            }
+                }
 
  
-            else { 
+                else { 
                    
                    
-                $acl.entries += $baseAclEntry
-                $JSONBody = $acl | ConvertTo-Json -Depth 10 
+                    $acl.entries += $baseAclEntry
+                    $JSONBody = $acl | ConvertTo-Json -Depth 10 
 
-                }
+                    }
             
 
-            $uri = $F5Session.BaseURL.Replace('/ltm/',"/apm/acl/~Common~$name")
-            $response = Invoke-RestMethodOverride -Method Patch -Uri $URI -Body $JSONBody -ContentType 'application/json' -WebSession $F5Session.WebSession
-            $response
+                $uri = $F5Session.BaseURL.Replace('/ltm/',"/apm/acl/~Common~$name")
+                $response = Invoke-RestMethodOverride -Method Patch -Uri $URI -Body $JSONBody -ContentType 'application/json' -WebSession $F5Session.WebSession
+                $response
+            }
         }
         
-}
+    }
 
 }
