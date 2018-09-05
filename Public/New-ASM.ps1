d52740a6905311c33c7ddcc7e995e9e77169ee5d
Klein <551479@bah.com>
2018-09-05 15:21:09 -0400

    finished function and tested

--- Public/New-ASM.ps1
+++ Public/New-ASM.ps1
@@ -47,7 +47,7 @@
       "uri": "/mgmt/tm/asm/policies",
       "body": {
         "type": "security",
-        "name": "test_asm",
+        "name": "$policyname",
         "caseInsensitive": false,
         "protocolIndependent": false,
         "partition": "Common",
@@ -76,7 +76,7 @@
             
             try{ 
                 $response = Invoke-RestMethodOverride -Method Post -Uri $URI -Body $JSONBody -ContentType 'application/json' -WebSession $F5Session.WebSession
-                Write-Output "New AMS policy creation task started..."
+                Write-Verbose "New AMS policy creation task started..."
             }
 
             catch { 
@@ -90,25 +90,31 @@
             
             $taskStatus = $false
 
+            Write-Verbose "Checking Task..."
+
             #check until completed
             while( $taskStatus -eq $false ){
                 try{ 
-                    Write-Output "Checking Task..."
+                    
                     $response = Invoke-RestMethodOverride -Method GET -Uri $URI -ContentType 'application/json' -WebSession $F5Session.WebSession
-                    if ( $response.result -eq "FAILURE" ){ Write-Output $response.errors }
-                    if ( $response.result -eq "COMPLETED" ) { $taskStatus = $true }
-                    Sleep -Seconds 3
+                    Write-Verbose "    [Status] $($response.status)"
+                    if ( $response.status -eq "FAILURE" ){ Write-Warning $response.errors; break; }
+                    if ( $response.status -eq "COMPLETED" ) { $taskStatus = $true }
+                    Sleep -Seconds 5
                 }
 
                 catch { 
                     Write-Warning "Error checking ASM policy task."
-                    $_.ErrorDetails.Message
+                    $response.errors
                     break
                 }
             }#while
 
+            # required to check for error due to the way break cmd works
+            if ( $response.status -eq "FAILURE" ){ break }
+
             #set uri to filer for newly create policy id
-            $uri = $F5Session.BaseURL.Replace('/ltm/',"asm/policies?`$filter=fullPath+eq+%27%2FCommon%2F$name%27&`$select=id") 
+            $uri = $F5Session.BaseURL.Replace('/ltm/',"/asm/policies?`$filter=fullPath+eq+%27%2FCommon%2F$policyname%27&`$select=id") 
 
             try{ 
                     
@@ -123,8 +129,118 @@
                     break
                 }
 
-            
-            $uri = $F5Session.BaseURL.Replace('/ltm/',"asm/policies/$id") 
+            #set uri for adding whitlist Ips
+            $uri = $F5Session.BaseURL.Replace('/ltm/',"/asm/policies/$id/whitelist-ips") 
+
+                    $json1 = @"
+
+                    {
+          "ipAddress": "128.229.4.2",
+          "ipMask": "255.255.255.255",
+          "description": "",
+          "trustedByPolicyBuilder": true,
+          "neverLearnRequests": false,
+          "neverLogRequests": false,
+          "ignoreAnomalies": false,
+          "ignoreIpReputation": false,
+          "blockRequests": "policy-default"
+        }
+"@
+
+                    $json2 = @"
+        {
+          "ipAddress": "156.80.4.2",
+          "ipMask": "255.255.255.255",
+          "description": "",
+          "trustedByPolicyBuilder": true,
+          "neverLearnRequests": false,
+          "neverLogRequests": false,
+          "ignoreAnomalies": false,
+          "ignoreIpReputation": false,
+          "blockRequests": "policy-default"
+        }
+"@
+
+                    $json3 = @"
+        {
+          "ipAddress": "54.225.156.133",
+          "ipMask": "255.255.255.255",
+          "description": "",
+          "trustedByPolicyBuilder": false,
+          "neverLearnRequests": false,
+          "neverLogRequests": false,
+          "ignoreAnomalies": false,
+          "ignoreIpReputation": false,
+          "blockRequests": "policy-default"
+        }
+"@
+
+
+
+                try{ 
+                    #add first exemption IP
+                    
+                    $response = Invoke-RestMethodOverride -Method POST -Uri $URI -ContentType 'application/json' -Body $json1 -WebSession $F5Session.WebSession
+                    Write-Verbose "`nAdded 128.229.4.2 to whitelist..."
+                    #add second exemption IP
+                    $response = Invoke-RestMethodOverride -Method POST -Uri $URI -ContentType 'application/json' -Body $json2 -WebSession $F5Session.WebSession
+                    Write-Verbose "Added 156.80.4.2 to whitelist..."
+                    $response = Invoke-RestMethodOverride -Method POST -Uri $URI -ContentType 'application/json' -Body $json3 -WebSession $F5Session.WebSession
+                    Write-Verbose "Added 54.225.156.133 with 'Don't Trust' to whitelist..."
+                }
+
+                catch { 
+                    Write-Warning "Error Adding Whitelist IP to ASM policy."
+                    $_.ErrorDetails.Message
+                    break
+                }
+
+                #set uri for patching readyness state
+                $uri = $F5Session.BaseURL.Replace('/ltm/',"/asm/policies/$id/general")
+                
+                $JSONBody = "{`"enforcementReadinessPeriod`":7}"
+                 
+                try{ 
+                    #patch readyness period
+                    $response = Invoke-RestMethodOverride -Method Patch -Uri $URI -ContentType 'application/json' -Body $JSONBody -WebSession $F5Session.WebSession
+                }
+
+                catch { 
+                    Write-Warning "Error Adding updating readyness period."
+                    $_.ErrorDetails.Message
+                    break
+                }
+
+                #set uri for patching ismodified state
+                $uri = $F5Session.BaseURL.Replace('/ltm/',"/asm/policies/$id")
+                
+                $JSONBody = "{`"isModified`":false}"
+                 
+                try{ 
+                    #patch ismodified state
+                    $response = Invoke-RestMethodOverride -Method Patch -Uri $URI -ContentType 'application/json' -Body $JSONBody -WebSession $F5Session.WebSession
+                }
+
+                catch { 
+                    Write-Warning "Error Adding updating ismodified state."
+                    $_.ErrorDetails.Message
+                    break
+                }
+
+                  #set uri for completed policy
+                $uri = $F5Session.BaseURL.Replace('/ltm/',"/asm/policies/$id")               
+                 
+                try{ 
+                    #get completed policy
+                    $response = Invoke-RestMethodOverride -Method GET -Uri $URI -ContentType 'application/json' -WebSession $F5Session.WebSession
+                    $response
+                }
+
+                catch { 
+                    Write-Warning "Error Retrieving ASM Policy."
+                    $_.ErrorDetails.Message
+                    break
+                }
 
            
 
