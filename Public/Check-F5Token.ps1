9d1fbfeec2f63160d1da10bc6a9bd460df7c1a91
Klein <551479@bah.com>
2018-10-18 10:35:56 -0400

    Adding Check token

---
+++ Public/Check-F5Token.ps1
@@ -0,0 +1,18 @@
+﻿function Check-F5Token {
+<#
+.SYNOPSIS
+    Graceuflly handles expired token attemps
+.NOTES
+    Requires F5-LTM modules from github
+#>
+   
+    #Handle expired or non-existing F5-Sessions gracefully
+    $exp = $F5Session.WebSession.Headers. 'Token-Expiration'
+    if ($exp -lt (date)) {
+
+      Write-Warning "F5 Session is not active or has expired." -ErrorAction Stop
+      break
+
+    }
+
+ }
