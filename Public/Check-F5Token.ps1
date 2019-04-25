c2cf5a89affd8e658171c11f8cabc0e032e96d25
Sky Klein <37423483+kleinsmk@users.noreply.github.com>
2019-04-25 17:04:21 -0400

    Update Check-F5Token.ps1

--- Public/Check-F5Token.ps1
+++ Public/Check-F5Token.ps1
@@ -8,7 +8,7 @@
    
     #Handle expired or non-existing F5-Sessions gracefully
     $exp = $F5Session.WebSession.Headers. 'Token-Expiration'
-    if ($exp -lt (date)) {
+    if ($exp -lt (get-date)) {
 
       Write-Warning "F5 Session is not active or has expired." -ErrorAction Stop
       break
