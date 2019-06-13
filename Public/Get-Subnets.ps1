7693d37d39b8ef67b1e795bb630ce3f254298923
Sky <kleinsmk@gmail.com>
2019-06-13 18:07:59 -0400

    Adding Get-Subnets.ps1

---
+++ Public/Get-Subnets.ps1
@@ -0,0 +1,47 @@
+﻿Function Get-Subnets {
+<#
+.SYNOPSIS
+    Returns all the subnets for a CR
+.Parameter aclName
+    Existing jira CR Ticket in format CR-####
+.Example
+    Get-Subnets -aclName AWS_30090989898
+
+    Returns all the subnets for ACL named AWS_30090989898
+.NOTES
+   
+    Requires F5-LTM, F5-APM
+#>
+    [cmdletBinding()]
+    param(
+        
+        #ACL Name
+        [Parameter(Mandatory=$true)]
+        [string]$aclName
+
+    )
+    begin {
+        
+             
+    }
+    process {
+       
+                try 
+                {
+                
+                    (Get-SingleAcl -name $aclName ).entries | Select-Object dstsubnet -Unique
+
+                }
+
+                catch 
+                {
+                        Write-Error $_.Exception.Message
+                        break
+                        
+                }
+
+                
+                 
+   }#end process
+
+}
